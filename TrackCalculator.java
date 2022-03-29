d082d2f1b1af9fca34ff0625c6733b398a233082
Troy Lopez <43734061+TLop503@users.noreply.github.com>
2022-03-29 11:22:12 -0700

    Created file, started to outline code.
    Added main(){}
    Added scanner
    added headers

---
+++ TrackCalculator.java
@@ -0,0 +1,14 @@
+/** This program is designed to help 
+*** Runners calculate how to pace various
+*** races in track or cross country
+*/
+
+//This package adds a scanner that can read user input
+import util.java.scanner.*;
+
+//Creates a scanner object that will detect input
+Scanner scan = new Scanner(System.in);
+
+//This is what the program will run
+public void main() {
+}
