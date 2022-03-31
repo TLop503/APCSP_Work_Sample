f98191738a7a9cc903e1dbd0ad6f0391a6c0f220
Troy Lopez <nicktheninja4me@gmail.com>
2022-03-31 10:55:26 -0700

    see prev commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # APCSP_Work_Sample
-Work Sample for APCSP Exam
+Written to meet the work sample requirement for the APCSP exam in 2022
+This program calculates reccomended paces and times for various races based on a user-inputed 400meter time
+
+
--- TrackCalculator.java
+++ TrackCalculator.java
@@ -6,48 +6,75 @@
 /*
 ** Time conversions for reference
 **  200 = 400/2 - 2
-**  800 = 400 + 2
-**  1500 = (400 * 4) - (400/4)
+**  800 = 400 * 2 + 2
+**  1500 = (400 * 4) - (400/4) + 6
 **  1600 = 400*4 + 8
-**  1 mile = 400*4 + 10
+**  1 mile = 400*4 + 11
 **  3000 = 
+**  5k = 
 */
 
 import java.util.Scanner; //Utility that can read user input
 
-class TrackCalculator 
-{
+class TrackCalculator {
     
     //Scanner that will read user input, used in nextLine
-    public Scanner scan = new Scanner(System.in);
+    public static Scanner scan = new Scanner(System.in);
     //lists that stores times:
-    public static int[] lTimes = {-1, -1, -1, -1, -1, -1, -1};
+    public static double[] calculatedTimes = {-1, -1, -1, -1, -1, -1, -1};
     //list that stores output texts:
-    public static final String[] lLables = 
+    public static final String[] kLables = 
     {
         "Your 400m Time: ", "800m Approx: ", "800m Approx: ", "1500m Approx: ",  
         "1600m Approx: ", "1 Mile Approx: ", "3k Approx: ", "5k Approx: "
     }; //this list was made on multiple lines for readability
 
-    public static void main(String[] args) //Main method, code is run in here
-    {
-        outputData();
+    //TODO: revise addon constant to be more realistic
+
+    //Stores what the user time gets multiplied by
+    public static final double[] kFactors = {0.5, 2, 3.75, 4, 4, 7.5, 12.5};
+
+    //Stores extra secconds added after multiplying
+    public static final double[] kAddon = {-2, 2, 6, 8, 11, 40, 60};
+
+    public static void main(String[] args){ //Main method, code is run in here
+    
+        //TODO: add print stuff
+        //hello, etc
+        //enter thing
+        String userInput = getNext();
+
+        //Checks if input is valid - matches returns true if input is only ints, otherise returns false
+        if (userInput.matches("[0-9]+")){
+            
+        } else {
+            //TODO: add message saying they typed it in wrong
+        }
+
     }
 
     /** Function to return the user input, using scanner */
-    public String getNext()
-    {
+    public static String getNext(){
         return scan.nextLine(); 
     }
 
     /** Function to output final times */
-    public static void outputData()
-    {
+    public static void outputData(){
         System.out.println("Reccomended paces for each race, based on your 400m time: ");
-        for (int i = 0; i < 7; i++) //iterates through lists of data
-        {
+        for (int i = 0; i < 7; i++){ //iterates through lists of data
+        
             //concatenates times with lables, prints to console
-            System.out.println(lLables[i] + lTimes[i]); 
+            System.out.println(kLables[i] + calculatedTimes[i]); 
         }
     }
+
+    /** Function to calculate final times */
+    public static void doMath(double time){
+        for (int i = 0; i < 7; i++) { //iterates through lists of data
+        
+            //TODO: replace this with modulus to seperate minutes and secconds
+            calculatedTimes[i] = ((time * kFactors[i]) + kAddon[i]);
+        }
+    }
+
 }
